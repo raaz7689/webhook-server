c3a6fa440811d072c8d0dfc46b367dfc4853f800
raaz7689 <peaterquill@gmail.com>
2025-04-29 17:34:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/webhook', (req, res) => {
-    console.log('Webhook received:', req.body);
-    res.status(200).send('Webhook received');
+app.post('/', (req, res) => {
+  console.log('Webhook received:', req.body);
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
-    console.log(`Webhook server is running on http://localhost:${PORT}`);
+  console.log(Server running on port ${PORT});
 });
