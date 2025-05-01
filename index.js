7d81e10ae3ae91d7e8844b00eab07305caf3e287
raaz7689 <peaterquill@gmail.com>
2025-05-01 15:01:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
 
-app.post('/', (req, res) => {
-  console.log('Webhook received:', req.body);
-  res.sendStatus(200);
+app.get("/", (req, res) => {
+  res.send("Webhook server is live!");
 });
 
+app.post("/webhook", (req, res) => {
+  console.log("Received webhook:", req.body);
+  res.status(200).send("Webhook received");
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(Server running on port ${PORT});
 });
