879ab5a2de30c2f5b3ea377715e8d6cefc16560b
raaz7689 <peaterquill@gmail.com>
2025-05-01 15:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.post("/webhook", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
